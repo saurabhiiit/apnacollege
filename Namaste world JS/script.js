3fd6151af5bc020e2c9ea8d8f668016d4f828709
saurabhiiit <saurabhagrahari487@gmail.com>
2022-05-30 07:58:12 +0530

    Working on Layout and JS demo

---
+++ Namaste world JS/script.js
@@ -0,0 +1,13 @@
+let namastebtn = document.querySelector('button');
+// namastebtn.addEventListener('click',showMsg);
+
+// function showMsg(){
+//     alert("Namaste World!");
+// }
+
+namastebtn.addEventListener('click',inputMsg);
+
+function inputMsg(){
+    let name = prompt('Enter Name of Student');
+    namastebtn.textContent = "Roll No. 1: " + name;
+}
